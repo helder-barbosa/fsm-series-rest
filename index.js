f613c5044d2348559323857f71afcab5dd4332cd
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-07-15 21:57:52 -0300

    CRUD in Rest

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express()
 const port = process.env.PORT || 3000
 const mongo = process.env.MONGO || 'mongodb://localhost/minhas-series-rest'
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 mongoose.Promise = global.Promise
-
 const series = require('./routes/series')
+
+app.use(bodyParser({ extended: true }))
 
 app.use('/series', series)
 
